dbc5a5e6e84a5f654952c621598bde0251056b2b
gyojincompany <luckyred@gmail.com>
2024-04-22 11:25:52 +0900

    로또 모든 회차 당첨번호 크롤링 결과 데이터프레임변환 기능 추가

--- lottoTest.py
+++ lottoTest.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 url = "https://dhlottery.co.kr/gameResult.do?method=byWin&drwNo=1116"
 
@@ -10,16 +11,23 @@ print(html)
 soup = BeautifulSoup(html, 'html.parser')
 
 date = soup.find('p', {'class':'desc'}).text  # 로또 추첨일
+lottoDate = datetime.strptime(date, "(%Y년 %m월 %d일 추첨)")
 
-print(date)
+print(lottoDate)
 
 lottoNumber = soup.find('div', {'class':'num win'}).find('p').text.strip().split('\n')  
 # 로또당첨번호 6개를 리스트로 변환하여 반환
 
-print(lottoNumber)
+lottoNumberList = []
 
-bonusNumber = soup.find('div', {'class':'num bonus'}).find('p').text.strip()  
-# 보너스번호 1개 반환
+for num in lottoNumber:
+    num = int(num)
+    lottoNumberList.append(num)
+
+print(lottoNumberList)
+
+bonusNumber = int(soup.find('div', {'class':'num bonus'}).find('p').text.strip())
+# 보너스번호 1개 반환 -> 정수로 변환
 
 print(bonusNumber)
 
